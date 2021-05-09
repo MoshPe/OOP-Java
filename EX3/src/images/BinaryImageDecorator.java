d58541a7c83b074bf55c05c0598b37e978c45968
MoshPe <fghghf98@gmail.com>
2021-05-09 11:35:50 +0300

    .

--- EX3/src/images/BinaryImageDecorator.java
+++ EX3/src/images/BinaryImageDecorator.java
@@ -2,17 +2,8 @@ package images;
 
 public abstract class BinaryImageDecorator extends BaseImage implements Image {
 
-<<<<<<< HEAD
 	public BinaryImageDecorator(int width, int height) {
 		super(width, height);
-=======
-	private int width,height;
-	private BaseImage a;
-	
-	public BinaryImageDecorator(int width, int height) {
-		this.width = width;
-		this.height = height;
-		a = new BaseImage(width,height);
->>>>>>> 45633c31d504332900d427aee1e83d3077797f13
 	}
+
 }
