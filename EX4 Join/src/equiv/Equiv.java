b6a8a11bc4c7fad6efce51ad7e411fa2f2b21f8f
MoshPe <fghghf98@gmail.com>
2021-05-27 23:40:36 +0300

    Update Equiv.java

--- EX4 Join/src/equiv/Equiv.java
+++ EX4 Join/src/equiv/Equiv.java
@@ -1,30 +1,40 @@
 package equiv;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 //This class represent an equilibrium department
 
 public class Equiv<E> {
-	private Set<Set<E>> list = new HashSet<>(); // set of sets
+	 private List<Set<E>> list = new ArrayList<>(); // list of sets
 
-	public void add(E e1, E e2) {
-		// get each set in the set, check if one of the types is in department
-		for (Set<E> set : list) { 
-			if (set.contains(e1) && !set.contains(e2))
-				set.add(e2);
-			else if (!set.contains(e1) && set.contains(e2))
-				set.add(e1);
-			else if (set.contains(e1) && set.contains(e2))
-				return;
-		}
-		// else, make new department with both of them
-		Set<E> temp = new HashSet<>();
-		temp.add(e2);
-		temp.add(e1);
-		list.add(temp);
-
-	}
+	 public void add(E e1, E e2) {
+            // get each set in the set, check if one of the types is in department
+            Set<E> e1Set = null, e2Set = null;
+            for (Set<E> set : list) {
+                if (set.contains(e1)) {
+                    set.add(e2);
+                    e1Set = set;
+                }
+                else if (set.contains(e2)) {
+                    set.add(e1);
+                    e2Set = set;
+                }
+            }
+            if(e1Set != null && e2Set != null){
+                e1Set.addAll(e2Set);
+                list.remove(e2Set);
+            }
+            // else, make new department with both of them
+            else if(e1Set == null && e2Set == null) {
+                Set<E> temp = new HashSet<>();
+                temp.add(e2);
+                temp.add(e1);
+                list.add(temp);
+            }
+        }
 
 	// check if e1 and e2 are in the same department
 	public boolean are(E e1, E e2) {
